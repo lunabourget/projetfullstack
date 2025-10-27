84d17561fabc059a2ddcc168419f1f319d914fbb
eighteenlebg <127135153+eighteenlebg@users.noreply.github.com>
2025-10-27 14:58:00 +0100

    added color db

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -13,6 +13,7 @@ CREATE TABLE users (
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    color VARCHAR(10),
     description TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
